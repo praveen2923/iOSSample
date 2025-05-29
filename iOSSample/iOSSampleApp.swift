755032ed88fa29804aa5cc8232448a253e5062bb
Praveen <phiremath@ssdi.sharp.co.in>
2025-05-29 23:20:49 +0530

    Initial Commit

---
+++ iOSSample/iOSSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSSampleApp.swift
+//  iOSSample
+//
+//  Created by praveen hiremath on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
