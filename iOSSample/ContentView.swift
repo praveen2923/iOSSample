67b2fd8e1200173395e1d15389a832b1bedf720c
praveen2923 <164493100+praveen2923@users.noreply.github.com>
2025-05-30 23:27:00 +0530

    Update ContentView.swift

--- iOSSample/ContentView.swift
+++ iOSSample/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world I am New to iOS!")
+            Text("Hello, world I am New to iOS! ")
         }
         .padding()
         
