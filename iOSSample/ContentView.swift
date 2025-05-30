94fcbc8df2cc5c40ecaf3022010b8864a3ca0bfc
Praveen <phiremath@ssdi.sharp.co.in>
2025-05-30 23:16:40 +0530

    add new file

--- iOSSample/ContentView.swift
+++ iOSSample/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Name Praveen")
+            Text("Name Praveen H")
         }
         .padding()
         
